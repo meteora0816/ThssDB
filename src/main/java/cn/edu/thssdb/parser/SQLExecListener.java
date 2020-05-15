75fe8a22916a753fe50838f3318c18a315173b74
swy20190 <K201410109@163.com>
2020-05-15 23:32:09 +0800

    more elegant to return resp. Good Night!

--- src/main/java/cn/edu/thssdb/parser/SQLExecListener.java
+++ src/main/java/cn/edu/thssdb/parser/SQLExecListener.java
@@ -4,13 +4,16 @@ import cn.edu.thssdb.rpc.thrift.Status;
 import cn.edu.thssdb.schema.Manager;
 import cn.edu.thssdb.rpc.thrift.ExecuteStatementResp;
 import cn.edu.thssdb.utils.Global;
+import com.sun.org.apache.bcel.internal.generic.GotoInstruction;
 
 
 import java.io.IOException;
 
 public class SQLExecListener extends SQLBaseListener {
     private Manager manager;
-    private ExecuteStatementResp resp;
+    private ExecuteStatementResp resp = new ExecuteStatementResp();
+    private Status status = new Status();
+    private boolean success = true;
 
     @Override
     public void enterParse(SQLParser.ParseContext ctx){
@@ -22,28 +25,36 @@ public class SQLExecListener extends SQLBaseListener {
     }
     @Override
     public void exitCreate_db_stmt(SQLParser.Create_db_stmtContext ctx) {
-        resp = new ExecuteStatementResp();
+        //resp = new ExecuteStatementResp();
         String dbName = ctx.database_name().getText();
         //System.out.println("dbname: "+dbName);
         try{
             if(manager.containDatabase(dbName)){
-                Status status = new Status(Global.FAILURE_CODE);
-                status.setMsg("Duplicated database name.");
-                resp.setStatus(status);
+                //Status status = new Status(Global.FAILURE_CODE);
+                success = success&&false;
+                status.setMsg(status.msg+"Duplicated database name.\n");
+                //resp.setStatus(status);
             }
             else {
                 manager.switchDatabase(dbName);
                 //manager.quit();
-                Status status = new Status(Global.SUCCESS_CODE);
-                status.setMsg("Database created successfully.");
-                resp.setStatus(status);
+                //Status status = new Status(Global.SUCCESS_CODE);
+                success = success&&true;
+                status.setMsg(status.msg+"Database created successfully.\n");
+                //resp.setStatus(status);
             }
         }catch (IOException e){
-            Status status = new Status(Global.FAILURE_CODE);
-            status.setMsg("Failed to create database file.");
+            //Status status = new Status(Global.FAILURE_CODE);
+            success = success&&false;
+            status.setMsg(status.msg+"Failed to create database file.\n");
             //System.out.println("Failed to create database");
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void exitDrop_db_stmt(SQLParser.Drop_db_stmtContext ctx) {
+
     }
 
     @Override
@@ -53,6 +64,13 @@ public class SQLExecListener extends SQLBaseListener {
 
 
     public ExecuteStatementResp getResult(){
+        if(success){
+            status.setCode(Global.SUCCESS_CODE);
+        }
+        else{
+            status.setCode(Global.FAILURE_CODE);
+        }
+        resp.setStatus(status);
         return resp;
     }
 }
